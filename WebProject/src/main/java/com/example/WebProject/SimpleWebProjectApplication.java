4eabbcfd47f5757783ad4e50b4388bafdbf3948a
souviksarkar9 <souviksarkar9@gmail.com>
2020-05-14 06:51:19 +0530

    final

--- WebProject/src/main/java/com/example/WebProject/SimpleWebProjectApplication.java
+++ WebProject/src/main/java/com/example/WebProject/SimpleWebProjectApplication.java
@@ -1,5 +1,5 @@
 package com.example.WebProject;
-
+  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
