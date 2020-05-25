6f3fb6e029e76b99985961d5adc4042385559277
souviksarkar9 <souviksarkar9@gmail.com>
2020-05-25 11:35:21 +0530

    for war

--- WebProject/src/main/java/com/example/WebProject/SimpleWebProjectApplication.java
+++ WebProject/src/main/java/com/example/WebProject/SimpleWebProjectApplication.java
@@ -1,18 +1,25 @@
 package com.example.WebProject;
-  
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SimpleWebProjectApplication {
+public class SimpleWebProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleWebProjectApplication.class, args);
 	}
-	
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SimpleWebProjectApplication.class);
+	}
+
 	@GetMapping("/")
 	public String getString() {
 		return "OK";
